088ab9d297cd5c0f09d75478d7fde0eacc317ddc
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-04-10 21:50:56 -0300

    Improve Package class

--- test_styro.py
+++ test_styro.py
@@ -1,13 +1,12 @@
 import os
 from pathlib import Path
+from subprocess import run
 
 import pytest
 from typer.testing import CliRunner
 
 from styro import __version__
 from styro.__main__ import app
-from styro._git import clone
-from styro._util import async_to_sync
 
 runner = CliRunner()
 
@@ -26,7 +25,7 @@ def test_styro() -> None:
     int(os.environ.get("FOAM_API", "0")) < 2112,  # noqa: PLR2004
     reason="requires OpenFOAM v2112 or later",
 )
-def test_basic_install() -> None:
+def test_install(tmp_path: Path) -> None:
     result = runner.invoke(app, ["uninstall", "reagency"])
     assert result.exit_code == 0
     assert "reagency" in result.stdout
@@ -38,6 +37,36 @@ def test_basic_install() -> None:
     result = runner.invoke(app, ["freeze"])
     assert result.exit_code == 0
     assert "reagency" in result.stdout
+
+    run(  # noqa: S603
+        ["git", "clone", "https://github.com/gerlero/reagency.git"],  # noqa: S607
+        cwd=tmp_path,
+        check=True,
+    )
+    result = runner.invoke(app, ["install", str(tmp_path / "reagency")])
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ["freeze"])
+    assert result.exit_code == 0
+    assert "reagency" in result.stdout
+    assert (tmp_path / "reagency").as_uri() in result.stdout
+
+    result = runner.invoke(app, ["install", "https://github.com/gerlero/reagency.git"])
+    assert result.exit_code == 0
+    assert "reagency" in result.stdout
+
+    result = runner.invoke(app, ["freeze"])
+    assert result.exit_code == 0
+    assert "reagency" in result.stdout
+    assert "https://github.com/gerlero/reagency.git" in result.stdout
+
+    result = runner.invoke(app, ["uninstall", "reagency"])
+    assert result.exit_code == 0
+    assert "reagency" in result.stdout
+
+    result = runner.invoke(app, ["freeze"])
+    assert result.exit_code == 0
+    assert "reagency" not in result.stdout
 
 
 @pytest.mark.skipif(
@@ -62,38 +91,6 @@ def test_package_with_dependencies() -> None:
     assert result.exit_code != 0
     assert "porousmicrotransport" in result.stdout
     assert "reagency" in result.stdout
-
-
-@pytest.mark.skipif(
-    int(os.environ.get("FOAM_API", "0")) < 2112,  # noqa: PLR2004
-    reason="requires OpenFOAM v2112 or later",
-)
-def test_install_from_git_repository() -> None:
-    result = runner.invoke(app, ["install", "https://github.com/gerlero/reagency.git"])
-    assert result.exit_code == 0
-    assert "reagency" in result.stdout
-
-    result = runner.invoke(app, ["freeze"])
-    assert result.exit_code == 0
-    assert "reagency" in result.stdout
-    assert "https://github.com/gerlero/reagency.git" in result.stdout
-
-
-@pytest.mark.skipif(
-    int(os.environ.get("FOAM_API", "0")) < 2112,  # noqa: PLR2004
-    reason="requires OpenFOAM v2112 or later",
-)
-async def test_install_local(tmp_path: Path) -> None:
-    async_to_sync(clone)(
-        tmp_path / "reagency", "https://github.com/gerlero/reagency.git"
-    )
-    result = runner.invoke(app, ["install", str(tmp_path / "reagency")])
-    assert result.exit_code == 0
-
-    result = runner.invoke(app, ["freeze"])
-    assert result.exit_code == 0
-    assert "reagency" in result.stdout
-    assert str(tmp_path / "reagency") in result.stdout
 
 
 def test_version() -> None:
